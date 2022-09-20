86db38b3e35548bd969bdc07d79f161fd6a0460a
Aleksandr <MrRotod@gmail.com>
2022-09-20 21:41:34 +0500

    прорешили первую задачу

---
+++ Seninar/DZ/Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿///Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзначное число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SeconDigit(int number) //метод вывод вророй цифры 
+{
+    int Dec = (number/10) % 10;
+    
+    return Dec;
+}
+
+Console.WriteLine ($"Вторая цифра от {number} - {SeconDigit(number)}");
